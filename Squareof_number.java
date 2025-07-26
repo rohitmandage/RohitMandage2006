825e886daa5153130f8ddeaa83b5bc5dbc279100
rohitmandage <rohitmandage2006@gmail.com>
2025-07-26 15:30:16 +0530

    no square

---
+++ Squareof_number.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Squareof_number {
+
+public static void main(String args[])   {
+
+int num=1;
+
+do{
+
+System.out.print(" square of ");
+System.out.print(num + ":");
+System.out.println(num*num);
+
+num++;
+}
+while (num<=10);
+  }
+  }
